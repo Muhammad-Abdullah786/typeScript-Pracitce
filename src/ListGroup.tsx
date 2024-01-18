f0a134416ec91c9390d1aa25ad6d4e74e4ae5f61
Muhammad Abdullah <abdulx125@gmail.com>
2024-01-19 00:34:58 +0500

    leaving typeScripts for time

--- src/ListGroup.tsx
+++ src/ListGroup.tsx
@@ -1,7 +1,13 @@
+import { MouseEvent } from "react";
+
 // for loop does not work in react we have to use map
 function ListGroup() {
   let group = ["tokyo", "new york", " Osaka", "konami", "FUji", "kaku"];
-  group = [];
+
+  const handler = (e: MouseEvent) => {
+    console.log(e.);
+  };
+  // group = [];
   //   to use loop we must use map
   //   if (group.length === 0) {
   //     return (
@@ -17,11 +23,20 @@ function ListGroup() {
     <>
       <h1>ListGoup</h1>
       {/* // cant be here */}
-      {group.length === 0 ? <p>NOt item here</p> : null}
+      {/* {group.length === 0 ? <p>NOt item here</p> : null} */}
+      {/* // ther is another way */}
+      {group.length === 0 && <p>NOt item here</p>}
       <ul className="list-group">
         {group.map((item, index) => {
           return (
-            <h6 key={index}>
+            <h6
+              className="list-group-item"
+              key={index}
+              onClick={(e) => {
+                // onClick e is called
+                console.log(e);
+              }}
+            >
               {index} {item}
             </h6>
           );
