4e0e918822a7f33a9e8f23aa47ff39d6d0332a42
Muhammad Abdullah <abdulx125@gmail.com>
2024-09-08 21:25:08 +0500

    alittle more about objects

---
+++ Objects.ts
@@ -0,0 +1,25 @@
+let user = {
+    usr:"abd",
+    age:33,
+    isActive:false
+}
+
+// function Obj({name:string ,isPaid:boolean}){}
+
+
+// Obj({name:"ken" ,isPaid:true})
+
+
+function fn():{name: string, email:string}{
+    return {name:"ek",email:'keeen'}
+}
+
+fn();
+
+function eee(user){
+return user
+}
+
+// eee(user)
+
+console.log(eee(user));
