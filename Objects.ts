97a1cf430789bb4aa37b9643778edaa2426810ad
Muhammad Abdullah <abdulx125@gmail.com>
2024-09-08 21:40:52 +0500

    type alias in ts

--- Objects.ts
+++ Objects.ts
@@ -22,4 +22,18 @@ return user
 
 // eee(user)
 
-console.log(eee(user));
+// console.log(eee(user));
+
+type User ={
+    name: string;
+    email:string;
+    isActive: false;
+}
+
+function createUser(user:User){}   // here user has a type User 
+
+
+
+
+
+export{}
